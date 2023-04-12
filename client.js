7a227924a32abf242781cfb05d55be163a55598c
Rida Batool <ridabatool.be13@gmail.com>
2023-04-12 19:41:01 -0400

    constants of IP and PORT

--- client.js
+++ client.js
@@ -1,14 +1,15 @@
 const net = require("net");
-
+const { IP, PORT } = require("./constants");
 // establishes a connection with the game server
 const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
+  //net.createConnection() expects the port argument first, 
+  //and the host (i.e. IP address) argument second.
+  const conn = net.createConnection(PORT, IP);
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+
   //The .on method lets you specify an event name 
   //and a function that does something when that event happens.
   conn.on("connect", () => {
